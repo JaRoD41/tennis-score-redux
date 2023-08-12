af8bb6c35ab449c0cc7886e610f43f293d96ff33
JaRoD41 <laurent.trinco@gmail.com>
2023-08-12 19:13:00 +0200

    add redux, folders and first components

---
+++ src/components/ActionsButtonRow.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function ActionsButtonRow() {
+  return (
+		<>
+			<div className="buttons-row">
+				<button className="button" id="reset">
+					Remettre à zéro
+				</button>
+				<button className="button" id="pause">
+					Pause / Reprendre
+				</button>
+			</div>
+		</>
+	)
+}
---
+++ src/components/PlayersButtonRow.jsx
@@ -0,0 +1,14 @@
+export default function PlayersButtonRow() {
+	return (
+		<>
+			<div className="buttons-row">
+				<button className="button" id="player-1">
+					Point Joueur 1
+				</button>
+				<button className="button" id="player-2">
+					Point Joueur 2
+				</button>
+			</div>
+		</>
+	)
+}
