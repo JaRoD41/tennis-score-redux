858325faeb0bd0551bcb0d50f336fbac9cb84eb8
JaRoD41 <laurent.trinco@gmail.com>
2023-08-13 10:38:10 +0200

    add logo

--- src/pages/MainScorePage.jsx
+++ src/pages/MainScorePage.jsx
@@ -1,10 +1,12 @@
 import ActionsButtonRow from '../components/ActionsButtonRow'
 import PlayersButtonRow from '../components/PlayersButtonRow'
 import ScoreDisplay from '../components/ScoreDisplay'
+import logo from '../assets/logo.webp'
 
 export default function MainScorePage() {
 	return (
 		<>
+			<img src={logo} className='logo' alt="logo" />
 			<ScoreDisplay />
 			<div className="buttons">
 				<PlayersButtonRow />
