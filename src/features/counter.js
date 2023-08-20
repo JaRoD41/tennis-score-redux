6400cc0d907f453c28741dda289d73297033a44c
JaRoD41 <laurent.trinco@gmail.com>
2023-08-20 22:22:57 +0200

    add tests to score features

--- src/features/counter.js
+++ src/features/counter.js
@@ -25,7 +25,25 @@ export const counter = createSlice({
 		},
 		// Je crée un reducer qui va ajouter un point au joueur qui a marqué
 		playerScored: (state, action) => {
-			state[action.payload.player]++
+			const player = action.payload.player
+			const otherPlayer = player === 'player1' ? 'player2' : 'player1'
+			if (state[player] === 40 && state[otherPlayer] === 40) {
+				state.advantage = player
+			} else if (state.advantage === player) {
+				state.winner = player
+				// state.playing = false
+			} else if (state.advantage === otherPlayer) {
+				state.advantage = null
+			} else if (state[player] === 30) {
+				state[player] = 40
+			} else if (state[player] === 15) {
+				state[player] = 30
+			} else if (state[player] === 0) {
+				state[player] = 15
+			} else if (state[player] === 40) {
+				state.winner = player
+				// state.playing = false
+			}
 		},
 		// Je crée un reducer qui va remettre à zéro le jeu
 		resetGame: (state) => {
@@ -33,11 +51,10 @@ export const counter = createSlice({
 			state.player2 = 0
 			state.advantage = null
 			state.winner = null
-		}
+		},
 	},
 })
 
-console.log('counter :', counter)
 // J'exporte les actions du reducer counter afin de les utiliser dans mon composant
 export const { playerScored, playPause, resetGame } = counter.actions
 export default counter.reducer
